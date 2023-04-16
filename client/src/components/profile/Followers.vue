d3fd6683e25b1e23e14989cbcd351daa8172eee0
Cx <85353920+CYAXXX@users.noreply.github.com>
2023-04-17 01:41:52 +0300

    Update Followers.vue

--- client/src/components/profile/Followers.vue
+++ client/src/components/profile/Followers.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="followers">
-        <h1>My Followers Page</h1>
+        <h1>Rûpela Şopînerên min</h1>
         <ul>
             <li v-for="person in followersList" :key="person.name">
-                <p>Name: {{ person.name }}</p>
-                <p>Username: {{ person.username }}</p>
-                <p>Mail: {{ person.mail }}</p>
+                <p>Nav: {{ person.name }}</p>
+                <p>Navê bikarhêner: {{ person.username }}</p>
+                <p>E-name: {{ person.mail }}</p>
                 
             </li>
         </ul>
